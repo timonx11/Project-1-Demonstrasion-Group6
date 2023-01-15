7e0cff5e7aecbbf5bbc7788edc7238ae156a5aa0
timonx11 <TimN@winslow.com.au>
2023-01-16 00:30:09 +1100

    update

--- starter_code/config.py
+++ starter_code/config.py
@@ -1,5 +1,7 @@
-# Nasdaq Data Link
-api_key = "vxTWE7cB573FdWfnpHLh"
 
 # Geoapify API Key
 geoapify_key = "26085283642c4a92be10b5e2b7fd74ad"
+
+# Nasdaq Data Link
+api_key = "vxTWE7cB573FdWfnpHLh"
+
---
+++ starter_code/config_tn.py
@@ -0,0 +1,5 @@
+
+# Geoapify API Key
+geoapify_key = "26085283642c4a92be10b5e2b7fd74ad"
+
+
